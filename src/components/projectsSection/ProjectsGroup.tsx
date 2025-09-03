61d8609daf34b8017f3ce3ccdcb21eca00b2b3a5
Saviour Agbor <saviouragbor1@gmail.com>
2025-09-03 10:17:25 +0100

    Add motion and integrate react scroll to projects section

--- src/components/projectsSection/ProjectsGroup.tsx
+++ src/components/projectsSection/ProjectsGroup.tsx
@@ -1,9 +1,20 @@
+import {motion} from "framer-motion"  
+import {fadeIn} from "../../framerMotion/variants"
+
 import SingleProject from "./SingleProject";
 const ProjectsGroup = ({ projects }) => {
   return (
     <div className="mt-10 flex flex-col">
       {projects.map((project, index) => {
-        return <SingleProject key={index} project={project} />;
+        return <motion.div
+          key={index}
+          variants={fadeIn(project.align === 'right' ? "left" : "right", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+        >
+          <SingleProject project={project} />;
+        </motion.div>
       })}
     </div>
   );
--- src/components/projectsSection/ProjectsText.tsx
+++ src/components/projectsSection/ProjectsText.tsx
@@ -1,14 +1,20 @@
-
+import {motion} from "framer-motion"  
+import {fadeIn} from "../../framerMotion/variants"
 const ProjectsText = () => {
   return (
-    <div className='mt- text-center md:max-w-[800px] mx-auto '>
+    <motion.div 
+      variants={fadeIn("up", 0.5)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.1 }}
+    className='mt- text-center md:max-w-[800px] mx-auto '>
       <h1 className="text-4xl font-bold text-cyan">My Projects</h1>
       <p className="text-lg text-white text-justify w-full px-8 py-6 shadow-lg shadow-lightGrey/20 rounded-xl">
         Here are some of the projects I have worked on, showcasing my skills in
         React, JavaScript, and TypeScript. Each project reflects my commitment to
         quality and user experience.
       </p>
-    </div>
+    </motion.div>
   )
 }
 
