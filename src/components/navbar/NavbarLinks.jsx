086a4f91a0d24f6ee04b393026c7fe82b9f57d0b
Saviour Agbor <saviouragbor1@gmail.com>
2025-09-07 11:09:57 +0100

    Add framer motion to Nav lists

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
+import { useMediaQuery } from "react-responsive";
 import { Link } from "react-scroll";
 
 const links = [
@@ -8,36 +11,38 @@ const links = [
   { link: "Contact", section: "contact" },
 ];
 
-// transition-all duration-500 flex flex-col w-full justify- md:flex-row gap-4 text-white absolute md:relative top-13 md:top-auto left-0 bg-cyan/40 py-2 md:bg-transparent backdrop-blur-2xl rounded-b-lg
-
-//     md:rounded-none items-center font-semibold text-lg shadow-[0_0_10px_cyan] md:shadow-none
-//     md:gap-4  md:left-auto md:backdrop-0 md:py-0 md:w-auto lg:text-md max-w-[600px] mx-auto
-
 const NavbarLinks = ({ toggleMenu }) => {
+  const isSmall = useMediaQuery({ maxWidth: 767 }); // sm screen
   return (
-    <ul className="flex absolute md:relative flex-col md:flex-row md:max-w-[500px] lg:mx-w-[500px] items-center justify-between md:mx-auto gap-0 md:gap-4 text-lg md:text-xl lg:text-2xl font-thin text-white top-8 md:top-auto md:bg-transparent bg-darkBrown w-[23%] md:w-full backdrop-blur-xl py-2 rounded-b-2xl md:rounded-none shadow-[0_0_10px_cyan] px-4 md:shadow-none right-5 overflow-hidden -z-1 md:z-20">
-      {links.map((link, index) => {
-        return (
-          <li
-            key={index}
-            className="group w-full text-center hover:scale-110 transition-all duration-1000 cursor-pointer"
+    <motion.ul
+      variants={isSmall ? fadeIn("down", 0.3) : undefined}
+      initial={isSmall ? "hidden" : false}
+      whileInView={isSmall ? "show" : false}
+      viewport={isSmall ? { once: false, amount: 0.01 } : undefined}
+      className="flex absolute md:relative flex-col md:flex-row md:max-w-[500px]
+     lg:mx-w-[500px] items-center justify-between md:mx-auto gap-2 md:gap-4 text-lg md:text-xl lg:text-2xl md:font-light text-white top5 md:top-auto md:bg-transparent bg-cyan/20 
+    w-full backdrop-blur-xl py-2 pt-14 md:pt-0 rounded-b-2xl md:rounded-none left-0 md:shadow-none right-5 overflow-hidden -z-1 md:z-20"
+    >
+      {links.map((link, index) => (
+        <li
+          key={index}
+          className="group w-full text-center hover:scale-110 transition-all duration-1000 cursor-pointer"
+        >
+          <Link
+            to={link.section}
+            smooth={true}
+            spy={true}
+            duration={1000}
+            offset={link.section === "skills" ? -30 : -75}
+            onClick={toggleMenu}
+            className="hover:text-cyan shadow-lg flex justify-center transition-all duration-500 cursor:pointer sm:block"
           >
-            <Link
-              to={link.section}
-              smooth={true}
-              spy={true}
-              duration={1000}
-              offset={link.section === "skills" ? -30 : -75}
-              onClick={toggleMenu}
-              className="hover:text-cyan transition-all duration-500 cursor:pointer sm:block"
-            >
-              {link.link}
-            </Link>
-            <div className="mx-auto bg-cyan/70 w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
-          </li>
-        );
-      })}
-    </ul>
+            {link.link}
+          </Link>
+          <div className="mx-auto bg-cyan/70 w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+        </li>
+      ))}
+    </motion.ul>
   );
 };
 
